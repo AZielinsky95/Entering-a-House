8b02edb396db556710533bd06127cf6ddd021c6e
Alejandro Zielinsky <azielinsky95@gmail.com>
2018-04-18 14:45:26 -0400

    Initial Commit

---
+++ Entering a House/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Entering a House
+//
+//  Created by Alejandro Zielinsky on 2018-04-18.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Entering a House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entering a House
+//
+//  Created by Alejandro Zielinsky on 2018-04-18.
+//  Copyright © 2018 Alejandro Zielinsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
